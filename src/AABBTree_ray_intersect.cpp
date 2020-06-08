9e5e787d0cf7a6661496c543abd9befc602422dc
Xiang <chenxiang00825@gmail.com>
2020-06-08 12:12:58 -0400

    AABBTree_ray_intersect

--- src/AABBTree_ray_intersect.cpp
+++ src/AABBTree_ray_intersect.cpp
@@ -8,10 +8,47 @@ bool AABBTree::ray_intersect(
   double & t,
   std::shared_ptr<Object> & descendant) const 
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  t = 0;
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+    t = 0;
+    if(!ray_intersect_box(ray, this->box, min_t, max_t)) return false;
+    
+    double left_t;
+    double right_t;
+    
+    std::shared_ptr<Object> left_descendant;
+    std::shared_ptr<Object> right_descendant;
+    
+    bool left_hit = this->left && this->left->ray_intersect(ray, min_t, max_t, left_t, left_descendant);
+    bool right_hit = this->right && this->right->ray_intersect(ray, min_t, max_t, right_t, right_descendant);
+    
+    if(left_hit && !left_descendant) left_descendant = this->left;
+    
+    if(right_hit && !right_descendant) right_descendant = this->right;
+    
+    if(left_hit && right_hit)
+    {
+        if(left_t < right_t)
+        {
+            t = left_t;
+            descendant = left_descendant;
+        }
+        else
+        {
+            t = right_t;
+            descendant = right_descendant;
+        }
+    }
+    else if(left_hit && !right_hit)
+    {
+        t = left_t;
+        descendant = left_descendant;
+    }
+    else if(!left_hit && right_hit)
+    {
+        t = right_t;
+        descendant = right_descendant;
+    }
+    else return false;
+    
+    return true;
 }
 
