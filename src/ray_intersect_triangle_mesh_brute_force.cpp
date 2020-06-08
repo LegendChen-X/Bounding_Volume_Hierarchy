1044a62bf788bed00d2b253b1704028fc0876570
Xiang <chenxiang00825@gmail.com>
2020-06-07 20:41:34 -0400

    ray_intersect_triangle_mesh_brute_force

--- src/ray_intersect_triangle_mesh_brute_force.cpp
+++ src/ray_intersect_triangle_mesh_brute_force.cpp
@@ -10,10 +10,30 @@ bool ray_intersect_triangle_mesh_brute_force(
   double & hit_t,
   int & hit_f)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  hit_t = 0;
-  hit_f = 0;
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+    hit_t = std::numeric_limits<double>::infinity();
+    int flag = 0;
+    
+    for(int i=0;i<F.rows();++i)
+    {
+        int A = F(i,0);
+        int B = F(i,1);
+        int C = F(i,2);
+        
+        double t_buff;
+        
+        if(ray_intersect_triangle(ray,V.row(A),V.row(B),V.row(C),min_t,max_t,t_buff))
+        {
+            if(t_buff<hit_t)
+            {
+                flag = 1;
+                hit_t = t_buff;
+                hit_f = i;
+            }
+        }
+    }
+    
+    if(flag) return true;
+    
+    return false;
+    
 }
