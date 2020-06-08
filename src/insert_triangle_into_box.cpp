dd7761774576bf7e7908589e9b38d4f1d9f1f899
Xiang <chenxiang00825@gmail.com>
2020-06-07 23:10:31 -0400

    insert_triangle_into_box

--- src/insert_triangle_into_box.cpp
+++ src/insert_triangle_into_box.cpp
@@ -1,14 +1,28 @@
 #include "insert_triangle_into_box.h"
 
+double maximum(double a,double b,double c,double d)
+{
+    double buff = a > b ? a : b;
+    double buff_s = buff > c ? buff : c;
+    return buff_s > d ? buff : d;
+}
+
+double minimum(double a,double b,double c,double d)
+{
+    double buff = a < b ? a : b;
+    double buff_s = buff < c ? buff : c;
+    return buff_s < d ? buff : d;
+}
+
 void insert_triangle_into_box(
   const Eigen::RowVector3d & a,
   const Eigen::RowVector3d & b,
   const Eigen::RowVector3d & c,
   BoundingBox & B)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+    for(int i=0;i<3;++i)
+    {
+        B.min_corner[i] = minimum(a[i],b[i],c[i],B.min_corner[i]);
+        B.max_corner[i] = maximum(a[i],b[i],c[i],B.max_corner[i]);
+    }
 }
-
-
