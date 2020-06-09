60965b4bae07b1d00f9ed16cad57dacebeff93c1
Xiang <chenxiang00825@gmail.com>
2020-06-08 22:49:27 -0400

    triangle_triangle_intersection

--- src/point_AABBTree_squared_distance.cpp
+++ src/point_AABBTree_squared_distance.cpp
@@ -1,6 +1,14 @@
 #include "point_AABBTree_squared_distance.h"
 #include <queue> // std::priority_queue
 
+/*
+ https://en.cppreference.com/w/cpp/container/priority_queue
+*/
+typedef struct Compare()
+{
+    bool operator()
+}compare;
+
 bool point_AABBTree_squared_distance(
     const Eigen::RowVector3d & query,
     const std::shared_ptr<AABBTree> & root,
@@ -9,9 +17,10 @@ bool point_AABBTree_squared_distance(
     double & sqrd,
     std::shared_ptr<Object> & descendant)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here
-  sqrd = 0;
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+    sqrd = std::numeric_limits<double>::infinity();
+    
+    std::priority_queue<std::pair<std::shared_ptr<AABBTree>,double>,std::vector<std::pair<std::shared_ptr<AABBTree>,double>>,compare > queue;
+    queue.emplace(root,point_box_squared_distance(query,root->box));
+    return false;
+    
 }
--- src/triangle_triangle_intersection.cpp
+++ src/triangle_triangle_intersection.cpp
@@ -8,8 +8,14 @@ bool triangle_triangle_intersection(
   const Eigen::RowVector3d & B1,
   const Eigen::RowVector3d & B2)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false; 
-  ////////////////////////////////////////////////////////////////////////////
+    double t;
+    
+    bool a = ray_intersect_triangle(Ray(A0,A1 - A0),B0,B1,B2,0.0,(A1-A0).norm(),t);
+    bool b = ray_intersect_triangle(Ray(A1,A2 - A1),B0,B1,B2,0.0,(A2-A1).norm(),t);
+    bool c = ray_intersect_triangle(Ray(A2,A0 - A2),B0,B1,B2,0.0,(A0-A2).norm(),t);
+    bool d = ray_intersect_triangle(Ray(B0,B1 - B0),A0,A1,A2,0.0,(B1-B0).norm(),t);
+    bool e = ray_intersect_triangle(Ray(B1,B2 - B1),A0,A1,A2,0.0,(B2-B1).norm(),t);
+    bool f = ray_intersect_triangle(Ray(B2,B0 - B2),A0,A1,A2,0.0,(B0-B2).norm(),t);
+    
+    return a || b || c || d || e || f;
 }
