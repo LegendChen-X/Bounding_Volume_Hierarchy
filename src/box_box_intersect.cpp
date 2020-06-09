00efc875e923deda9a1f04349b59b690dca44745
Xiang <chenxiang00825@gmail.com>
2020-06-08 22:52:39 -0400

    box_box_intersect

--- src/box_box_intersect.cpp
+++ src/box_box_intersect.cpp
@@ -3,9 +3,10 @@ bool box_box_intersect(
   const BoundingBox & A,
   const BoundingBox & B)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+    bool a = A.min_corner[0] < B.max_corner[0] && B.min_corner[0] < A.max_corner[0];
+    bool b = A.min_corner[1] < B.max_corner[1] && B.min_corner[1] < A.max_corner[1];
+    bool c = A.min_corner[2] < B.max_corner[2] && B.min_corner[2] < A.max_corner[2];
+    
+    return a & b & c;
 }
 
