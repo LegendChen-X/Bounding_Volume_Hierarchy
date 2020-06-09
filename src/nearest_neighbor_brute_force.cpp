95b7f892aa070951a1cde2e96c2f054ddbc5517b
Xiang <chenxiang00825@gmail.com>
2020-06-08 20:13:36 -0400

    nearest_neighbor_brute_force

--- src/nearest_neighbor_brute_force.cpp
+++ src/nearest_neighbor_brute_force.cpp
@@ -7,9 +7,19 @@ void nearest_neighbor_brute_force(
   int & I,
   double & sqrD)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  I = -1;
-  sqrD = 0;
-  ////////////////////////////////////////////////////////////////////////////
+    I = -1;
+    sqrD = std::numeric_limits<double>::infinity();;
+    
+    for(int i=0;i<points.rows();++i)
+    {
+        double x = points(i,0) - query[0];
+        double y = points(i,1) - query[1];
+        double z = points(i,2) - query[2];
+        double buff = x * x + y * y + z * z;
+        if(buff < sqrD)
+        {
+            sqrD = buff;
+            I = i;
+        }
+    }
 }
